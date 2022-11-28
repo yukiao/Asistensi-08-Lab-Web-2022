c7c341338dbccfed19161912c8397131991bf1fe
aimlana <salim.yaklu@gmail.com>
2022-11-28 12:50:00 +0800

    Kumpul tugas 8

---
+++ H071211019/Pertemuan-8/database/seeders/MahasiswaSeeder.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Mahasiswa;
+use Faker\Factory as Faker;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class MahasiswaSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $faker = Faker::create('id_ID');
+
+        for($i = 1; $i <= 1000; $i++){
+            Mahasiswa::create([
+                'nim' => $faker->unique()->bothify('?0########'),
+                'nama' => $faker->name(),
+                'alamat' => $faker->address(),
+                'fakultas' => $faker->words(2, true)
+            ]);
+        }
+    }
+}
