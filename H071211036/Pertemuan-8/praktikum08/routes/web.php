eb521f8fcd87228d8ebeda54af3f3d6e7ba25eb4
isranirwana <isranirwana2409@gmail.com>
2022-12-16 10:27:07 +0800

    Tugas Praktikum 8 dan 9

---
+++ H071211036/Pertemuan-8/praktikum08/routes/web.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\MahasiswaController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/mahasiswa', [MahasiswaController::class, 'index'])->name('mahasiswa');
+Route::get('/tambahmahasiswa', [MahasiswaController::class, 'tambahmahasiswa'])->name('tambahmahasiswa');
+Route::post('/insertdata', [MahasiswaController::class, 'insertdata'])->name('insertdata');
+
+Route::get('/tampilkandata/{id}', [MahasiswaController::class, 'tampilkandata'])->name('tampilkandata');
+Route::put('/updatedata/{id}', [MahasiswaController::class, 'updatedata'])->name('updatedata');
+Route::get('/delete/{id}', [MahasiswaController::class, 'delete'])->name('delete');
